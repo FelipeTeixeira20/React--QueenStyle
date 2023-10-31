ed5b20f27bc7b00bde3f69b24e511f8890584afe
Felipe A Teixeira <felipe.ateixeira8@gmail.com>
2023-10-30 21:31:31 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+    <header>
+      <img 
+        src="image/QUEENSTYLE.jpg" alt="QUEENSTYLE"
+        width="1318"
+        height="auto"
+      />
+      <a href='https://www.pensador.com/frase/Mjc4NjQ5Mw/'>LOGIN</a>
+    </header>
+    <h1>Hello</h1>
+    </html>
   );
 }
 
