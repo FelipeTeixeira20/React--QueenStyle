1b4fae8fb08955551658ac22e74b343d7e0a6c31
Barbara Diogo <barbara2diogo@gmail.com>
2023-11-01 09:27:51 -0300

    Melhorias no navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import './font-awesome/css/font-awesome.min.css';
 import Slider from './Slider';
 
 
 function App() {
+  const [isSearchInputVisible, setSearchInputVisible] = useState(false);
+
+  const toggleSearchInput = () => {
+    setSearchInputVisible(!isSearchInputVisible);
+  };
+
   return (
     <html>
       <body>
         <header>
           <img 
             src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
-            width="1318"
+            width="100%"
             height="100"
           />
         </header>
-        <div className="navbar">
-      <div className="categories">
-        <span>Jeans</span>
-        <span>Feminino</span>
-        <span>Masculino</span>
-        <span>Infantil</span>
-        <span>Ofertas</span>
-      </div>
-      </div>
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li className="navbar-item"><a href="#">Feminino</a></li>
+            <li className="navbar-item"><a href="#">Masculino</a></li>
+            <li className="navbar-item"><a href="#">Infantil</a></li>
+            <li className="navbar-item"><a href="#">Promoção</a></li>
+            <li className="navbar-item search-icon">
+              <i className="fa fa-search" onClick={() => toggleSearchInput()}></i>
+              <div className="search-input">
+                <input type="text" placeholder="Pesquisar..." />
+              </div>
+            </li>
+          </ul>
+        </nav>
       </body>
     </html>
   );
