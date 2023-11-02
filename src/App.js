1b315948b263a7f9005b85feaae691c958ea7634
Barbara Diogo <barbara2diogo@gmail.com>
2023-11-01 21:57:39 -0300

    Melhorias no navbar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import './font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css'; // Importe os estilos do Font Awesome
 import Slider from './Slider';
 
-
 function App() {
-  const [isSearchInputVisible, setSearchInputVisible] = useState(false);
-
-  const toggleSearchInput = () => {
-    setSearchInputVisible(!isSearchInputVisible);
-  };
-
   return (
-    <html>
-      <body>
-        <header>
-          <img 
-            src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
-            width="100%"
-            height="100"
-          />
-        </header>
-        <nav className="navbar">
-          <ul className="navbar-list">
-            <li className="navbar-item"><a href="#">Feminino</a></li>
-            <li className="navbar-item"><a href="#">Masculino</a></li>
-            <li className="navbar-item"><a href="#">Infantil</a></li>
-            <li className="navbar-item"><a href="#">Promoção</a></li>
-            <li className="navbar-item search-icon">
-              <i className="fa fa-search" onClick={() => toggleSearchInput()}></i>
-              <div className="search-input">
-                <input type="text" placeholder="Pesquisar..." />
-              </div>
-            </li>
-          </ul>
-        </nav>
-      </body>
-    </html>
+    <div>
+      <header>
+        <img 
+          src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
+          width="100%"
+          height="100"
+        />
+      </header>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item"><a href="#">Feminino</a></li>
+          <li className="navbar-item"><a href="#">Masculino</a></li>
+          <li className="navbar-item"><a href="#">Infantil</a></li>
+          <li className="navbar-item"><a href="#">Promoção</a></li>
+        </ul>
+        <div className="no-bullet">
+          <li className="navbar-item search-icon">
+              <input type="text" placeholder="Pesquisar..." />
+              <i className="fa fa-search"></i>
+              <i className="fa fa-heart"></i>
+              <i className="fa fa-user"></i>
+          </li>
+        </div>
+      </nav>
+    </div>
   );
 }
+
 export default App;
