d0c37c10b593fd564103370d9caa9c158d6118fe
Felipe A Teixeira <felipe.ateixeira8@gmail.com>
2023-11-03 18:35:18 -0300

    Subindo arquivos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css'; // Importe os estilos do Font Awesome
 import Slider from './Slider';
-
 
 function App() {
   return (
-    <html>
-      <body>
-        <header>
-          <img 
-            src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
-            width="1318"
-            height="100"
-          />
-        </header>
-        <div className="navbar">
-      <div className="categories">
-        <span>Jeans</span>
-        <span>Feminino</span>
-        <span>Masculino</span>
-        <span>Infantil</span>
-        <span>Ofertas</span>
-      </div>
-      </div>
-      </body>
-    </html>
+    <div>
+      <header>
+        <img 
+          src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
+          width="100%"
+          height="100"
+        />
+      </header>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item"><a href="#">Feminino</a></li>
+          <li className="navbar-item"><a href="#">Masculino</a></li>
+          <li className="navbar-item"><a href="#">Infantil</a></li>
+          <li className="navbar-item"><a href="#">Promoção</a></li>
+        </ul>
+        <div className="no-bullet">
+          <li className="navbar-item search-icon">
+              <input type="text" placeholder="Pesquisar..." />
+              <i className="fa fa-search"></i>
+              <i className="fa fa-heart"></i>
+              <i className="fa fa-user"></i>
+          </li>
+        </div>
+      </nav>
+    </div>
   );
 }
+
 export default App;
