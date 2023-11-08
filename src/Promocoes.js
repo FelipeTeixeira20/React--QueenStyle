29fef1e915600e7d82eaafb7dab1c063900c40ee
Felipe A Teixeira <felipe.ateixeira8@gmail.com>
2023-11-07 22:02:17 -0300

    Atualizando pagina inicial

---
+++ src/Promocoes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Promocoes.css'; // Certifique-se de importar o arquivo CSS necessário
+
+class RadialMenu extends React.Component {
+  render() {
+    return (
+      <div>
+        <span><span></span></span>
+        <div className="wrap">
+          <a href="#"><div></div></a>
+          <a href="#"><div></div></a>
+          <a href="#"><div></div></a>
+          <a href="#"><div></div></a>
+          <a href="#"><div></div></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RadialMenu;
