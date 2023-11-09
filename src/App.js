4dbd3b211932ad27b3b69a1a2854f5817f0b51e6
HigorBellini <higorbellini2000@gmail.com>
2023-11-08 22:12:33 -0300

    Higor: Substituição da logotipo QUEENSTYLE.jpg por uma .png

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import 'font-awesome/css/font-awesome.min.css'; // Importe os estilos do Font Awesome
-import ImageCarousel from './ImageCarousel';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css"; // Importe os estilos do Font Awesome
+import ImageCarousel from "./ImageCarousel";
 
 function App() {
-
   const images = [
-    'image/Homens.png',
-    'image/Mulheres.png',
-    'image/mulheres2.png',
+    "image/Homens.png",
+    "image/Mulheres.png",
+    "image/mulheres2.png",
   ];
   return (
     <div>
-      <header>
-        <img 
-          src="image/QUEENSTYLE.jpeg" alt="QUEENSTYLE"
-          width="100%"
-          height="100"
+      <header className="cabecalho-principal-QUEENSTYLE">
+        <img
+          className="img-QUEENSTYLE-cabecalho-principal"
+          src="image/QUEENSTYLE.png"
+          alt="QUEENSTYLE"
         />
       </header>
       <nav className="navbar">
         <ul className="navbar-list">
-          <li className="navbar-item"><a href="#">Feminino</a></li>
-          <li className="navbar-item"><a href="#">Masculino</a></li>
-          <li className="navbar-item"><a href="#">Infantil</a></li>
-          <li className="navbar-item"><a href="#">Promoção</a></li>
+          <li className="navbar-item">
+            <a href="#">Feminino</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Masculino</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Infantil</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Promoção</a>
+          </li>
         </ul>
         <div className="no-bullet">
           <li className="navbar-item search-icon">
-              <input type="text" placeholder="Pesquisar..." />
-              <i className="fa fa-search"></i>
-              <i className="fa fa-heart"></i>
-              <i className="fa fa-user"></i>
-              <i className="fa fa-brazilian-real-sign"></i>
+            <input type="text" placeholder="Pesquisar..." />
+            <i className="fa fa-search"></i>
+            <i className="fa fa-heart"></i>
+            <i className="fa fa-user"></i>
+            <i className="fa fa-brazilian-real-sign"></i>
           </li>
         </div>
       </nav>
@@ -40,21 +47,19 @@ function App() {
         <ImageCarousel images={images} />
       </div>
       <div className="aplicativo">
-      <div className="column">
-        <button className="button">Coleções</button>
-        <button className="button">Coleções</button>
+        <div className="column">
+          <button className="button">Coleções</button>
+          <button className="button">Coleções</button>
+        </div>
+        <div className="column">
+          <button className="button">Coleções</button>
+          <button className="button">Coleções</button>
+        </div>
+        <div className="columnD">
+          <button className="buttonD">Destaques</button>
+        </div>
       </div>
-      <div className="column">
-        <button className="button">Coleções</button>
-        <button className="button">Coleções</button>
-      </div>
-      <div className="columnD">
-        <button className="buttonD">Destaques</button>
-      </div>
-
     </div>
-    </div>
-    
   );
 }
 
